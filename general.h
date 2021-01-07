c20d724015b5dec63500954cf23df2793aeb97c2
nmaixner <75439681+nmaixner@users.noreply.github.com>
2021-01-07 19:50:20 +0200

    Add files via upload
    changing create inst function
    creating commands 1-8

--- general.h
+++ general.h
@@ -6,19 +6,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define REGISTER_ADDRESS_LEN 1
-#define OPPCODE_LEN 2
-#define LINE_LEN (OPPCODE_LEN+3*REGISTER_ADDRESS_LEN+2)
+#define LINE_LEN 7
 
 #define NUMBER_OF_REGISTERS 16
 #define MAX_TIME 
 
 //instruction struct : 
 typedef struct Instruction {
-	int opcode; // 8 bits
-	int rd; // 4 bits
-	int rs; // 4 bits
-	int rt; // 4 bits
+	unsigned int opcode; // 8 bits
+	unsigned int rd; // 4 bits
+	unsigned int rs; // 4 bits
+	unsigned int rt; // 4 bits
 	int immediate; // 20 bits
 	int isType2; // flag for instructions using immediate
 }Inst;
@@ -26,9 +24,6 @@ typedef struct Instruction {
 // create new instruction:
 Inst* initInstruction();
 
-int register2Val(char currentLine, int whichReg);
-
-void applyOpcode(Inst* currentInstruction, ) // #complete
 
 
 
